7173c2951b015e05eb76c5b19bd07f261554452c
Rudolf <rudolfvictoria8@gmail.com>
2026-08-21 11:31:04 +0800

    Original

---
+++ Cinema/Ticket_Selling_System.C
@@ -0,0 +1,452 @@
+
+
+// PROBLEM: PAANO MADIDISPLAY ANG MGA MOVIES SA TRANSACTION SUMMARY
+
+// FEATURES TO ADD: 
+    /*
+        discount
+        sukli 
+        payment validation
+    
+    */
+
+#include <stdio.h>
+
+void header();
+
+void buyTicket(int *choice, int *quantity, int *total);
+
+void schedule(int *choice, int *time);
+
+void orderSummary(int total, int time);
+
+void mode_payment(int *choice
+                , int *amount
+                , int *card
+                , int total
+                , int quantity
+                , char card1[]
+                , char card2[]);
+
+void transactionSummaryCASH(int quantity, int total, int amount);
+
+void transactionSummaryCard1(int quantity, int total, char card1[]);
+
+void transactionSummaryCard2(int quantity, int total, char card2[]);
+
+int main() {
+
+    int movieChoice = 0;
+    int ticketChoice = 0;
+    int scheduleChoice = 0;
+    int paymentChoice = 0;
+    int quantity = 0;
+    int total = 0;
+    int time = 0;
+    int amount = 0;
+    int card = 0;
+
+    char card1[] = "BPI";
+    char card2[] = "BDO";
+
+
+    // Pipili ng Movie to Watch
+    header();
+    printf("\n1.) Spider-Man: Brand New Day"
+           "\n2.) Moana"
+           "\n3.) Oppenheimer"
+           "\n4.) The Odyssey"
+           "\n5.) Toy Story 5\n");
+    
+    printf("\nCHOOSE MOVIE(1-5): ");
+    scanf("%d", &movieChoice);
+
+    // Checks if tama input
+    while (movieChoice > 5 || movieChoice < 1)
+    {
+        printf("\nInvalid Input!\n");
+        printf("Please Try Again\n");
+        header();
+        printf("\n1.) Spider-Man: Brand New Day"
+                "\n2.) Moana"
+                "\n3.) Oppenheimer"
+                "\n4.) The Odyssey"
+                "\n5.) Toy Story 5\n");
+    
+    printf("\nCHOOSE MOVIE(1-5): ");
+    scanf("%d", &movieChoice);
+    }
+    
+    switch (movieChoice)
+    {
+    case 1:
+        header();
+        buyTicket(&ticketChoice, &quantity, &total);           
+        header();
+        schedule(&scheduleChoice, &time);
+
+        printf("\n========== ORDER DETAILS ==========\n");
+        printf("\nMovie\t: Spider-Man: Brand New Day");
+        orderSummary(total, time);
+        mode_payment(&paymentChoice, &amount, &card, total, quantity, card1, card2);
+        
+        if (paymentChoice == 1)
+        {
+            printf("\n\nPAYMENT SUCCESSFUL!\n");
+            printf("\n========== ORDER DETAILS ==========\n");
+            printf("\nMovie: Spider-Man: Brand New Day");
+            transactionSummaryCASH(quantity, total, amount);
+        }
+
+        else if (paymentChoice == 2)
+        {
+            if (card == 1)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: Spider-Man: Brand New Day");
+                transactionSummaryCard1(quantity, total, card1);
+            }
+            else if (card == 2)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: Spider-Man: Brand New Day");
+                transactionSummaryCard2(quantity, total, card2);
+            }
+          
+        }
+        
+        break;
+    
+
+    case 2:
+        header();
+        buyTicket(&ticketChoice, &quantity, &total);           
+        header();
+        schedule(&scheduleChoice, &time);
+
+        printf("\n========== ORDER DETAILS ==========\n");
+        printf("\nMovie\t: Moana");
+        orderSummary(total, time);
+        mode_payment(&paymentChoice, &amount, &card, total, quantity, card1, card2);
+
+        if (paymentChoice == 1)
+        {
+            printf("\n\nPAYMENT SUCCESSFUL!\n");
+            printf("\n========== ORDER DETAILS ==========\n");
+            printf("\nMovie: Moana");
+            transactionSummaryCASH(quantity, total, amount);
+        }
+
+        else if (paymentChoice == 2)
+        {
+            if (card == 1)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: Moana");
+                transactionSummaryCard1(quantity, total, card1);
+            }
+            else if (card == 2)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: Moana");
+                transactionSummaryCard2(quantity, total, card2);
+            }
+          
+        }
+        
+        break;
+
+    case 3:
+        header();
+        buyTicket(&ticketChoice, &quantity, &total);           
+        header();
+        schedule(&scheduleChoice, &time);
+
+        printf("\n========== ORDER DETAILS ==========\n");
+        printf("\nMovie\t: Oppenheimer");
+        orderSummary(total, time);
+        mode_payment(&paymentChoice, &amount, &card, total, quantity, card1, card2);
+
+        if (paymentChoice == 1)
+        {
+            printf("\n\nPAYMENT SUCCESSFUL!\n");
+            printf("\n========== ORDER DETAILS ==========\n");
+            printf("\nMovie: Oppenheimer");
+            transactionSummaryCASH(quantity, total, amount);
+        }
+
+        else if (paymentChoice == 2)
+        {
+            if (card == 1)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: Oppenheimer");
+                transactionSummaryCard1(quantity, total, card1);
+            }
+            else if (card == 2)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: Oppenheimer");
+                transactionSummaryCard2(quantity, total, card2);
+            }
+          
+        }
+        
+        break;
+
+    case 4:
+        header();
+        buyTicket(&ticketChoice, &quantity, &total);           
+        header();
+        schedule(&scheduleChoice, &time);
+
+        printf("\n========== ORDER DETAILS ==========\n");
+        printf("\nMovie\t: The Odyssey");
+        orderSummary(total, time);
+        mode_payment(&paymentChoice, &amount, &card, total, quantity, card1, card2);
+
+        if (paymentChoice == 1)
+        {
+            printf("\n\nPAYMENT SUCCESSFUL!\n");
+            printf("\n========== ORDER DETAILS ==========\n");
+            printf("\nMovie: The Odyssey");
+            transactionSummaryCASH(quantity, total, amount);
+        }
+
+        else if (paymentChoice == 2)
+        {
+            if (card == 1)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: The Odyssey");
+                transactionSummaryCard1(quantity, total, card1);
+            }
+            else if (card == 2)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: The Odyssey");
+                transactionSummaryCard2(quantity, total, card2);
+            }
+          
+        }
+        
+        break;
+
+    case 5:
+        header();
+        buyTicket(&ticketChoice, &quantity, &total);           
+        header();
+        schedule(&scheduleChoice, &time);
+
+        printf("\n========== ORDER DETAILS ==========\n");
+        printf("\nMovie\t: Toy Story 5");
+        orderSummary(total, time);
+        mode_payment(&paymentChoice, &amount, &card, total, quantity, card1, card2);
+
+        if (paymentChoice == 1)
+        {
+            printf("\n\nPAYMENT SUCCESSFUL!\n");
+            printf("\n========== ORDER DETAILS ==========\n");
+            printf("\nMovie: Toy Story 5");
+            transactionSummaryCASH(quantity, total, amount);
+        }
+
+        else if (paymentChoice == 2)
+        {
+            if (card == 1)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: Toy Story 5");
+                transactionSummaryCard1(quantity, total, card1);
+            }
+            else if (card == 2)
+            {
+                printf("\n\nPAYMENT SUCCESSFUL!\n");
+                printf("\n========== TRANSACTION DETAILS ==========\n");
+                printf("\nMovie: Toy Story 5");
+                transactionSummaryCard2(quantity, total, card2);
+            }
+          
+        }
+        
+        break;
+
+
+    }
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+void header() 
+{
+    printf("\n========== CINEMA TICKET ==========\n");
+}
+
+void buyTicket(int *ticketChoice, int *quantity, int *total) 
+{
+        printf("\nTICKET PRICES: \n"
+           "\n1. Regular - PHP 620"
+           "\n2. Duo(PROMO!) - PHP 1200\n");
+        printf("\nCHOOSE PACKAGE(1-2): ");
+        scanf("%d", ticketChoice);
+    
+        while (*ticketChoice > 2 || *ticketChoice < 1)
+        {   
+            printf("\nInvalid Input!");
+            printf("\nPlease Try Again\n");
+            printf("\nTICKET PRICES: \n"
+           "\n1. Regular - PHP 620"
+           "\n2. Duo(PROMO!) - PHP 1200\n");
+        printf("\nCHOOSE PACKAGE(1-2): ");
+        scanf("%d", ticketChoice);
+        }
+        
+        printf("\nHOW MANY TICKET(S)/PACKAGE(S): ");
+        scanf("%d", quantity);
+        
+        if (*ticketChoice == 1)
+        {
+            *total = *quantity * 620;
+        }
+        else if (*ticketChoice == 2)
+        {
+            *total = *quantity * 1200;
+        }  
+  
+}
+
+void schedule(int *scheduleChoice, int *time) 
+{
+    printf("\n1.) 12:00 PM"
+           "\n2.) 4:00 PM"
+           "\n3.) 7:00 PM\n");
+    printf("\nSELECT TIME(1-3): ");
+    scanf("%d", scheduleChoice);
+
+    while (*scheduleChoice > 3 || *scheduleChoice < 1)
+    {
+        printf("\nINVALID INPUT!"
+                "\nPlease Try Again\n");
+        printf("\n1.) 12:00 PM"
+           "\n2.) 4:00 PM"
+           "\n3.) 7:00 PM\n");
+    printf("\nSELECT TIME(1-3): ");
+    scanf("%d", scheduleChoice);
+   
+    }
+
+    if (*scheduleChoice == 1)
+    {
+        *time = 12;
+    }
+    else if (*scheduleChoice == 2)
+    {
+        *time = 4;
+    }
+    else if (*scheduleChoice == 3)
+    {
+        *time = 7;
+    }
+    
+}
+void orderSummary(int total, int time) 
+{
+    printf("\nTime\t: %d:00 PM", time);
+    printf("\nTotal\t: PHP %d ", total);
+
+}
+
+void mode_payment(int *paymentChoice
+                , int *amount
+                , int *card
+                , int total
+                , int quantity
+                , char card1[]
+                , char card2[]) 
+{
+    printf("\n\n1. CASH"
+           "\n2. CARD"
+           "\n\nHOW WOULD YOU LIKE TO PAY(1-2): ");
+           scanf("%d", paymentChoice);
+    
+        while (*paymentChoice > 2 || *paymentChoice < 0)
+        {
+            printf("\n\nInvalid Input!"
+                   "\n Please Try Again");
+
+            printf("\n\n1. CASH"
+                   "\n2. CARD"
+                   "\n\nHOW WOULD YOU LIKE TO PAY(1-2): ");
+            scanf("%d", paymentChoice);
+        }
+
+        if (*paymentChoice == 1)
+        {
+            printf("\nENTER AMOUNT: ");
+            scanf("%d", amount);
+
+
+        }
+
+        else if (*paymentChoice == 2)
+        {
+            printf("\nACCEPTED CARDS: "
+                    "\n\n1. BPI"
+                    "\n2. BDO");
+            printf("\n\nENTER CARD(1-2): ");
+            scanf("%d", card);
+
+            while (*card > 2 || *card < 1)
+            {
+                printf("\nInvalid Input!"
+                       "\nPlease Try Again\n");
+                printf("\nACCEPTED CARDS: "
+                    "\n\n1. BPI"
+                    "\n2. BDO");
+            printf("\n\nENTER CARD(1-2): ");
+            scanf("%d", card);
+            }
+            
+
+        }
+        
+}
+
+void transactionSummaryCASH(int quantity, int total, int amount) {
+    
+    printf("\nTICKET(S) PURCHASED: %d", quantity);
+    printf("\nTOTAL: PHP %d", total);
+    printf("\nPAYMENT RECEIVED: PHP %d\n\n", amount);
+}
+
+void transactionSummaryCard1(int quantity, int total, char card1[]) {
+    
+    printf("\nTICKET(S)/PACKAGE(S) PURCHASED: %d", quantity);
+    printf("\nTOTAL: PHP %d", total);
+    printf("\nMODE OF PAYMENT: CARD");
+    printf("\nBANK CARD: %s\n\n", card1);
+}
+
+void transactionSummaryCard2(int quantity, int total, char card2[]) {
+
+    printf("\nTICKET(S)/PACKAGE(S) PURCHASED: %d", quantity);
+    printf("\nTOTAL: PHP %d", total);
+    printf("\nMODE OF PAYMENT: CARD");
+    printf("\nBANK CARD: %s\n\n", card2);
+}
