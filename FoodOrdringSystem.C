d9d475a1664520e75cc3cd152fc7311fefc7b34c
Rudolf <rudolfvictoria8@gmail.com>
2026-08-31 09:44:07 +0800

    Added Structure

--- FoodOrdringSystem.C
+++ FoodOrdringSystem.C
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <windows.h>
 
+
 void pressToContinue() {
     printf("=============================================\n");
     printf("            EXPRESS DINER KIOSK              \n");
@@ -28,7 +29,7 @@ int getUserFoodChoice() {
 
     int foodChoice = 0;
 
-    printf("=============================================");
+    printf("\n=============================================");
     printf("\n   ------ WELCOME TO EXPRESS DINER! ------");
     printf("\n=============================================\n");
     printf("\n[1] MEALS - Enjoy Hot Rice Meals!");
@@ -42,14 +43,61 @@ int getUserFoodChoice() {
     return foodChoice;
 }
 
+void invalidInput() {
+
+    printf("\nInvalid Input");
+    printf("\nPlease Try Again\n");
+}
+
 int main() {
 
     // Cinema Food Ordering System
     int foodChoice = 0;
+    int successfulOrder = 0;
 
     pressToContinue();
 
-    foodChoice = getUserFoodChoice();
+    do
+    {
+        foodChoice = getUserFoodChoice();
+
+            if (foodChoice == 1)
+            {
+                successfulOrder = 0;
+            }
+
+            else if (foodChoice == 2)
+            {
+                successfulOrder = 0;
+            }
+
+            else if (foodChoice == 3)
+            {
+                successfulOrder = 0;
+            }
+
+            else if (foodChoice == 4)
+            {
+                successfulOrder = 0;
+            }
+
+            else if (foodChoice == 5)
+            {
+                successfulOrder = 1;
+            }
+
+            else {
+                system("cls");
+                invalidInput();
+                successfulOrder = 0;
+            }
+            
+            
+
+    } while (successfulOrder != 1);
+    
+    system("cls");
+    
 
     return 0;
 }
